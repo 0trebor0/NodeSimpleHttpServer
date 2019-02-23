8cbdaf4267fb265c79a52213ad7c3feeb4437674
0trebor0 <robertsantos98@hotmail.co.uk>
2019-02-23 14:17:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,35 +4,55 @@ const mime = require('mime-types');
 const url = require('url');
 let port = 80;
 let htdocs = __dirname+'/htdocs';
-let methods = {};
-const server = http.createServer();
-server.listen(port);
-server.on('listening', ()=>{
-    console.log("Server Started on PORT:"+port);
-});
-server.on('request', ( req, res )=>{
-    let urlParsed = url.parse(req.url);
-    console.log("REQUEST: "+urlParsed.path);
-    if(urlParsed.pathname == '/'){
-        methods.sendFile( res, "/index.html");
-    }else{
-        methods.sendFile( res, urlParsed.pathname );
-    }
-});
-methods.sendFile = ( res, file )=>{
-    let filePath = htdocs+file;
-    if( fs.existsSync( htdocs+file ) ){
-        let fileStream = fs.createReadStream( filePath );
-        fileStream.pipe( res );
-        fileStream.on('close', ()=>{
-            res.end();
-        });
-    }else{
-        methods.notFound( res );
+try{
+    const server = http.createServer();
+    server.listen(port);
+    server.on('listening', ()=>{
+        greenColour("Server Started on PORT:"+port);
+    });
+    server.on(  'request', ( req, res )=>{
+        greenColour( "[REQUEST][IP: "+req.connection.remoteAddress+" ] "+req.url );
+        let urlparsed = url.parse(req.url, true);
+        let query = urlparsed.query;
+        if( urlparsed.pathname == '/'){
+            streamFile( res, htdocs+"/index.html" );
+        } else {
+            streamFile( res, htdocs+urlparsed.pathname );
+        }
+    } );
+}catch( err ){
+    redColour( err );
+}
+greenColour = ( text )=>{
+    console.log("\x1b[32m",text);
+}
+redColour = ( text )=>{
+    console.log("\x1b[31m",text);
+}
+streamFile = ( res, file )=>{
+    try{
+        if( fs.existsSync( file ) ){
+            let fileStream = fs.createReadStream( file );
+            fileStream.pipe( res );
+            fileStream.on('close', ()=>{
+                res.end();
+            });
+        } else {
+            redColour( "File: "+file+" not found" );
+            notFound( res, file );
+        }
+    }catch( err ){
+        redColour( err );
+        serverIternalServerError( res, err);
     }
 }
-methods.notFound = ( res )=>{
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<head><title>Not Found</title></head><body><center><h1>File Not Found</h1></center></body>");
+notFound = ( res, file )=>{
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    res.write( "<center><h1>404 Not Found</h1><p>"+file+" not found</p></center>" );
+    res.end();
+}
+serverIternalServerError = ( res, error )=>{
+    res.writeHead(500, {'Content-Type': 'text/html'});
+    res.write( "<center><h1>500 Internal Server Error<h1><p>"+error+"</p></center>" );
     res.end();
 }
